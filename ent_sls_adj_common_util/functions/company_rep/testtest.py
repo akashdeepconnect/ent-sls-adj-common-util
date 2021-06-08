88031243a3d3b4e6774f77e7dca87f724565e6b3
akashdeepconnect <akashdeep.connect@gmail.com>
2021-06-08 14:06:44 +0530

    4th commit

---
+++ ent_sls_adj_common_util/functions/company_rep/testtest.py
@@ -0,0 +1,11 @@
+import sys
+import os
+path = os.path.abspath(__file__)
+dir_path = os.path.dirname(path)
+sys.path.insert(0,dir_path)
+
+import sys
+sys.path.insert(0, '.')
+
+import sys
+sys.path.insert(0, '.')
